58b374427de173aa0dde3800b884cfd8a9d8d152
JP <jgpeters717@gmail.com>
2022-11-05 15:11:31 -0400

    initial commit w tailwind and some basic styles

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE: string = 'My personal website.';
+export const SITE_DESCRIPTION: string = 'Welcome to my website!';
+export const CURRENT_YEAR: string = new Date().getFullYear().toString();
+export const COPYRIGHT: string = `© ${CURRENT_YEAR} joshua peters. all rights reserved.`
